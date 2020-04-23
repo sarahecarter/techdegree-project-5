23ba1ba8d5d6073199a69949f2dd09a512919f55
Sarah Carter <scarter3@conncoll.edu>
2020-04-23 17:05:14 -0400

    This commit will fix a typo in the JavaScript search function

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ const imageTexts = document.querySelectorAll('a[data-sub-html]');
 const imageSearch = event => {
     const searchTerm = event.target.value.toLowerCase();
 
-    imageTexts.forEach(imageText => {
-        const text = imageText.textContent.toLowerCase();
-        const image = imageTextWhen.nextElementSibling;
+    imageTexts.forEach(imageTexts => {
+        const text = imageTexts.textContent.toLowerCase();
+        const image = imageTexts.nextElementSibling;
 
         if(text.indexOf(searchTerm) > -1) {
             image.style.display = "block";
